145e2850df400f8975b2ac99bf351b56ee3e6213
Kirill Smotrov <kirya.smotrov.02@mail.ru>
2021-07-28 23:27:35 +0300

    Diploy

--- InternApp/settings.py
+++ InternApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-a1q5&*sa^b0)c@j5rhf=e%=%4y#wlket6pimz&*)t28y@5jjrb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,3 +129,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn InternApp.wsgi --log-file -
